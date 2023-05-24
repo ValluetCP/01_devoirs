dca3618fe124ea221762d1810ed95fb239e79ae6
Cynthia <cynthia.petitot@gmail.com>
2023-05-24 23:13:34 +0200

    devoir js

---
+++ devoir_javascript/devoir_js_1/js/devoir_js_1.js
@@ -0,0 +1,34 @@
+
+// var monNom = "petitot";
+// var monNom = prompt();
+
+// console.log(monNom);
+// document.write(monNom);
+// alert(monNom);
+
+// console.log(typeof monNom);
+
+// var numberOne = prompt();
+
+// console.log(numberOne);
+// document.write(numberOne);
+// alert(numberOne);
+// console.log(typeof numberOne);
+
+var unChiffre = 126.68;
+console.log(unChiffre);
+console.log(typeof unChiffre);
+
+// unChiffre = unChiffre.toString();
+// console.log(typeof unChiffre);
+
+unChiffre = parseInt(unChiffre);
+console.log(unChiffre);
+console.log(typeof unChiffre);
+
+unChiffre = parseFloat(unChiffre);
+console.log(unChiffre);
+console.log(typeof unChiffre);
+
+
+
